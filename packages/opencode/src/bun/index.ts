6b309153fe129eb10683079705102f54b63610ad
Fabio <fdittz@gmail.com>
2026-01-23 11:20:41 -0300

    Adding --backend=copyfile for NFS installations

--- packages/opencode/src/bun/index.ts
+++ packages/opencode/src/bun/index.ts
@@ -92,6 +92,7 @@ export namespace BunProc {
       "add",
       "--force",
       "--exact",
+      "--backend=copyfile",
       // TODO: get rid of this case (see: https://github.com/oven-sh/bun/issues/19936)
       ...(proxied() ? ["--no-cache"] : []),
       "--cwd",
